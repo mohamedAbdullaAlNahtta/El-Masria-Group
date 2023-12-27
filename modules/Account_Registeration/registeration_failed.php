c5beb0d092a17a2e85cf23c4522a3d2f604eecf1
mohamedAbdullaAlNahtta <mohamedAbdullaAlNahtta@mohamed.abdulla.1@gmail.com>
2023-12-27 17:03:02 +0200

    done

---
+++ modules/Account_Registeration/registeration_failed.php
@@ -0,0 +1,252 @@
+
+<!DOCTYPE html>
+<html id="html-page" lang="en">
+
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- // This frameWork  created bye ENG Muhammad Abdullah El Nahtta/////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // you can contact me through mobile number 201093001070 or lanline 20 48 2327352 -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- //////////////////////////////&&&&&&&&&&&&&&&&,@///////////////////////////////// -->
+<!-- ////////////////////////////&&&&&&&&&&&&&&&&&@&&&%&%///////////////////////////// -->
+<!-- ///////////////////////////*%&%&&%%%%#%%%%%%&%&&&&&&&//////////////////////////// -->
+<!-- ///////////////////////////&&&/,,,,,,...*,*,,***//(&&&%////////////////////////// -->
+<!-- /////////////////////////%&%&,,.....,,,,,,,,,,****//%&&////////////////////////// -->
+<!-- /////////////////////////%%#*,..... .........,,,,***/#&&///////////////////////// -->
+<!-- /////////////////////////&#*...........,...,,,,,,,****%&///////////////////////// -->
+<!-- /////////////////////////%(,....,...,......,,,###((***/&&*/////////////////////// -->
+<!-- ///////////////////////..,#*,&#/,****@&*,,%/((#((/((*@#*,*/////////////////////// -->
+<!-- ///////////////////////.,..,@,,*(.&@(/,/.,@/((((*//*,*%**//////////////////////// -->
+<!-- /////////////////////////,,(,/..,,**,./..,**@,**,%&***//*//////////////////////// -->
+<!-- //////////////////////////.(,....,,,.....,******,,,,*/#,.//////////////////////// -->
+<!-- ////////////////////////////(....,(...*..,*//***/#*//%/////////////////////////// -->
+<!-- /////////////////////////////%#,,//#(///((((#*//%%&/(%&////////////////////////// -->
+<!-- /////////////////////////////#/#/*(..,,,,*,*//**#/%&&./////////////////////////// -->
+<!-- ///////////////////////////////%#*/,...,,///****#&&&///////////////////////////// -->
+<!-- ///////////////////////////////,*%%#/(*//#(###%&&#/*,//////////////////////////// -->
+<!-- //////////////////////////////,&..*/&&%&&&&&&&&#***,.&&////////////////////////// -->
+<!-- ////////////////////////////@%///(....,,*///*****,//..&&&&&&&&&&@//////////////// -->
+<!-- ///////////////////////&&&&&%%%/////*...,,,*,,,////...&&&&&&&&&&&&&&&&&&&&&////// -->
+<!-- ///////////////&%%%%%%%&&&%%%%%/////////,,,,////////.,&&&&&&&&&&&&&&&&&&&&&&&&&// -->
+<!-- /////////%%%&&&%%%%%%%%%%&%%%%%%//////@&#%%&&&&%//...,&&&&&&&%&&&&%%%&&%&&&&&&&// -->
+<!-- //////%&&&%%%%%%%###%%%#%%%%%%%./////,(/%#&&&&,.,# ...(&&&&&&&%&&&%%%%%%%%%%%&&// -->
+<!-- //////%%%%%%%#######%####%%%%%%%///.../*%#&&&,....///.*&&&&%&%%&&&&&%%%%%%%%%&&// -->
+<!-- //////%%################%%%%%%%&.////////%%%&,..../////&&&%&%%%%&&&&&%%%%&&%&&%// -->
+<!-- //////%%###(#(((#(#(####%%%%%%%&,///////%%%&&&.../////.&&&&%%%%%&&&&&&&&&&&&&&&// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // you can contact me through mobile number 201093001070 or lanline 20 48 2327352 -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- // This frameWork  created bye ENG Muhammad Abdullah El Nahtta/////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!-- Favicon icon -->
+        <link rel="icon" type="../../dark/image/png" sizes="16x16" href="../../assets/images/favicon.png" />
+        <title>El Masria Group Developments</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="../../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <!-- Custom CSS -->
+        <link id="custom-css" href="../../thems/white/css/style.css" rel="stylesheet"/>
+        <!-- login CSS -->
+        <link id="login-css" href="../../thems/white/login.css" rel="stylesheet"/>
+    </head>
+
+    <body onload="typeWriter()">
+        <div id="particles-js">
+            <!-- ============================================================== -->
+            <!-- Preloader - style you can find in spinners.css -->
+            <!-- ============================================================== -->
+            <div class="preloader">
+                <svg class="circular" viewBox="25 25 50 50">
+                    <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+                </svg>
+            </div>
+            <!-- ============================================================== -->
+            <!-- Main wrapper - style you can find in pages.scss -->
+            <!-- ============================================================== -->
+            <section id="wrapper">
+                <div class="login-register">
+                    <div class="row login-box">
+                        <div id="right-card-block-login" class="col-lg-12 b-l card-block">
+                            <form class="form-horizontal" id="loginform" method="post" action="login.php">
+                                <center>
+                                    <h2 class="box-title m-b-20" style="margin-bottom: 0;"><img id="mylogoImg2" src="../../assets/images/El Masria Group Logo 2.png" /></h2>
+                                </center>
+                                <center>
+                                    <h2 class="box-title m-b-20" style="margin-bottom: 0;"><img id="mylogoImg2" width="100%" src="image/red.jpg"></h2>
+                                </center>
+
+                                
+                                <br />
+                            </form>
+                           
+                        </div>
+                       
+                  
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Wrapper -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        <script src="../../assets/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap tether Core JavaScript -->
+        <script src="../../assets/plugins/bootstrap/js/tether.min.js"></script>
+        <script src="../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <!-- slimscrollbar scrollbar JavaScript -->
+        <script src="../../thems/white/js/jquery.slimscroll.js"></script>
+        <!--Wave Effects -->
+        <script src="../../thems/white/js/waves.js"></script>
+        <!--Menu sidebar -->
+        <script src="../../thems/white/js/sidebarmenu.js"></script>
+        <!--stickey kit -->
+        <script src="../../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+        <!--Custom JavaScript -->
+        <script src="../../thems/white/js/custom.min.js"></script>
+        <!-- ============================================================== -->
+        <!-- Style switcher -->
+        <!-- ============================================================== -->
+        <script src="../../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+
+        <!-- Na7tta Canvas  -->
+        <script src="../../canvas/particles.js"></script>
+        <script src="../../canvas/js/app.js"></script>
+
+        <!-- script that reset forms after submit and never store again when i refresh page-->
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+            }
+        </script>
+        <script>
+        function myFunctionShowPass() {
+                var li = document.getElementById("showPass").className;
+                if (li === "mdi mdi-eye") {
+                    document.getElementById("showPass").setAttribute("class", "mdi mdi-eye-off");
+                    document.getElementById("new_password").setAttribute("type", "text");
+                } else if (li === "mdi mdi-eye-off") {
+                    document.getElementById("showPass").setAttribute("class", "mdi mdi-eye");
+                    document.getElementById("new_password").setAttribute("type", "password");
+                }
+            }
+            function myFunctionCompareNewPassWithConfirmPass() {
+                var newPasswordUserInput = document.getElementById("new_password").value;
+                var confirmPasswordUserInput = document.getElementById("confirm_password").value;
+                if (newPasswordUserInput === confirmPasswordUserInput) {
+                    document.getElementById("incorrectRetype").style.display = "none";
+                    document.getElementById("correctRetype").style.display = "block";
+                    return true;
+                } else {
+                    document.getElementById("incorrectRetype").style.display = "block";
+                    document.getElementById("correctRetype").style.display = "none";
+                    return false;
+                }
+            }
+        </script>
+        <script>
+            function changeThemeStyle() {
+                var x = document.getElementById("changeTheme").value;
+                var z = document.getElementById("currentLang").innerHTML;
+                if (x === "dark") {
+                    document.getElementById("html-page").setAttribute("dir", "ltr");
+                    document.getElementById("custom-css").setAttribute("href", "../../thems/dark/css/style.css");
+                    document.getElementById("login-css").setAttribute("href", "../../thems/dark/login.css");
+                    document.getElementById("mylogoImg").src = "../../assets/images/El Masria Group Logo 2022-10-White.png";
+                    document.getElementById("mylogoImg2").src = "../../assets/images/El Masria Group Logo 2-white.png";
+                } else if (x === "white") {
+                    document.getElementById("html-page").setAttribute("dir", "ltr");
+                    document.getElementById("custom-css").setAttribute("href", "../../thems/white/css/style.css");
+                    document.getElementById("login-css").setAttribute("href", "../../thems/white/login.css");
+                    document.getElementById("mylogoImg").src = "../../assets/images/El Masria Group Logo 2022-10.png";
+                    document.getElementById("mylogoImg2").src = "../../assets/images/El Masria Group Logo 2.png";
+                }
+            }
+
+        </script>
+        <?php 
+        if(isset($_GET['theme'])){
+            ?>
+            <script>
+            function changeThemeStyleLoginPage() {
+                if ("dark" === "<?php echo $_GET['theme']; ?>") {
+                    document.getElementById("html-page").setAttribute("dir", "ltr");
+                    document.getElementById("custom-css").setAttribute("href", "../../thems/dark/css/style.css");
+                    document.getElementById("login-css").setAttribute("href", "../../thems/dark/login.css");
+                    document.getElementById("mylogoImg").src = "../../assets/images/El Masria Group Logo 2022-10-White.png";
+                    document.getElementById("mylogoImg2").src = "../../assets/images/El Masria Group Logo 2-white.png";
+                } else if ("white" === "<?php echo $_GET['theme']; ?>") {
+                    document.getElementById("html-page").setAttribute("dir", "ltr");
+                    document.getElementById("custom-css").setAttribute("href", "../../thems/white/css/style.css");
+                    document.getElementById("login-css").setAttribute("href", "../../thems/white/login.css");
+                    document.getElementById("mylogoImg").src = "../../assets/images/El Masria Group Logo 2022-10.png";
+                    document.getElementById("mylogoImg2").src = "../../assets/images/El Masria Group Logo 2.png";
+                }
+            }
+            changeThemeStyleLoginPage();
+
+        </script>
+
+
+            <?php
+
+        }
+        
+        ?>
+    </body>
+
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- // This frameWork  created bye ENG Muhammad Abdullah El Nahtta/////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // you can contact me through mobile number 201093001070 or lanline 20 48 2327352 -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- //////////////////////////////&&&&&&&&&&&&&&&&,@///////////////////////////////// -->
+<!-- ////////////////////////////&&&&&&&&&&&&&&&&&@&&&%&%///////////////////////////// -->
+<!-- ///////////////////////////*%&%&&%%%%#%%%%%%&%&&&&&&&//////////////////////////// -->
+<!-- ///////////////////////////&&&/,,,,,,...*,*,,***//(&&&%////////////////////////// -->
+<!-- /////////////////////////%&%&,,.....,,,,,,,,,,****//%&&////////////////////////// -->
+<!-- /////////////////////////%%#*,..... .........,,,,***/#&&///////////////////////// -->
+<!-- /////////////////////////&#*...........,...,,,,,,,****%&///////////////////////// -->
+<!-- /////////////////////////%(,....,...,......,,,###((***/&&*/////////////////////// -->
+<!-- ///////////////////////..,#*,&#/,****@&*,,%/((#((/((*@#*,*/////////////////////// -->
+<!-- ///////////////////////.,..,@,,*(.&@(/,/.,@/((((*//*,*%**//////////////////////// -->
+<!-- /////////////////////////,,(,/..,,**,./..,**@,**,%&***//*//////////////////////// -->
+<!-- //////////////////////////.(,....,,,.....,******,,,,*/#,.//////////////////////// -->
+<!-- ////////////////////////////(....,(...*..,*//***/#*//%/////////////////////////// -->
+<!-- /////////////////////////////%#,,//#(///((((#*//%%&/(%&////////////////////////// -->
+<!-- /////////////////////////////#/#/*(..,,,,*,*//**#/%&&./////////////////////////// -->
+<!-- ///////////////////////////////%#*/,...,,///****#&&&///////////////////////////// -->
+<!-- ///////////////////////////////,*%%#/(*//#(###%&&#/*,//////////////////////////// -->
+<!-- //////////////////////////////,&..*/&&%&&&&&&&&#***,.&&////////////////////////// -->
+<!-- ////////////////////////////@%///(....,,*///*****,//..&&&&&&&&&&@//////////////// -->
+<!-- ///////////////////////&&&&&%%%/////*...,,,*,,,////...&&&&&&&&&&&&&&&&&&&&&////// -->
+<!-- ///////////////&%%%%%%%&&&%%%%%/////////,,,,////////.,&&&&&&&&&&&&&&&&&&&&&&&&&// -->
+<!-- /////////%%%&&&%%%%%%%%%%&%%%%%%//////@&#%%&&&&%//...,&&&&&&&%&&&&%%%&&%&&&&&&&// -->
+<!-- //////%&&&%%%%%%%###%%%#%%%%%%%./////,(/%#&&&&,.,# ...(&&&&&&&%&&&%%%%%%%%%%%&&// -->
+<!-- //////%%%%%%%#######%####%%%%%%%///.../*%#&&&,....///.*&&&&%&%%&&&&&%%%%%%%%%&&// -->
+<!-- //////%%################%%%%%%%&.////////%%%&,..../////&&&%&%%%%&&&&&%%%%&&%&&%// -->
+<!-- //////%%###(#(((#(#(####%%%%%%%&,///////%%%&&&.../////.&&&&%%%%%&&&&&&&&&&&&&&&// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // you can contact me through mobile number 201093001070 or lanline 20 48 2327352 -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- // This frameWork  created bye ENG Muhammad Abdullah El Nahtta/////////////////// -->
+<!-- // Powered by ENG Muhammad Abdullah El Nahtta//////////////////////////////////// -->
+<!-- ///////////////////////////////////////////////////////////////////////////////// -->
+
+</html>
