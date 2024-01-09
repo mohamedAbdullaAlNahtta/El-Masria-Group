d0ecc762f342178a759c691dc73e7cb06ee216e4
mohamedAbdullaAlNahtta <mohamedAbdullaAlNahtta@mohamed.abdulla.1@gmail.com>
2024-01-09 08:39:47 +0200

    Update add_new_client.php

--- api/add_new_client.php
+++ api/add_new_client.php
@@ -1,47 +1,51 @@
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "el-masria-group";
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "el-masria-group";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+
+ if ($_SERVER["REQUEST_METHOD"] === "POST") {
+     $Username = $_SERVER["PHP_AUTH_USER"];
+     $password = $_SERVER["PHP_AUTH_PW"];
+
+     if (
+         $Username === "el-masria-group-portal" &&
+         $password === "asdA125sdw1dSf5rtertPref"
+     ) {
+         $national_ID = $_REQUEST["national_ID"];
+         $full_name = $_REQUEST["full_name"];
+         $email = $_REQUEST["email"];
+         $phone_number = $_REQUEST["phone_number"];
+
+         $sql = "INSERT INTO `client_user` (national_ID, full_name, email, phone_number, reg_status)
+          VALUES ('$national_ID', '$full_name', '$email', '$phone_number', 'not registered')";
+         $result = $conn->query($sql);
+
+         if ($result == 1) {
+             $data["message"] = "data saved successfully";
+             $data["status"] = "Ok";
+         } else {
+             $data["message"] = "data failed to be saved";
+             $data["status"] = "error";
+         }
+         $conn->close();
+     }
+
+     else{
+      $data["message"] = "Authentication failed";
+      $data["status"] = "error";
+     }
+
+ } else {
+     $data["message"] = "Format not supported";
+     $data["status"] = "error";
+ }
+ echo json_encode($data);
 
 
+    
 
-if ($_SERVER["REQUEST_METHOD"] === 'POST')
-{
-    $Username = $_SERVER["PHP_AUTH_USER"];
-    $Username = $_SERVER['PHP_AUTH_PW'];
-
-    $national_ID = $_REQUEST["national_ID"];
-    $full_name = $_REQUEST["full_name"];
-    $email = $_REQUEST["email"];
-    $phone_number = $_REQUEST["phone_number"];
-
-    $sql = "INSERT INTO `client_user ` (national_ID, full_name, email, phone_number, reg_status)
-    VALUES ('$national_ID', '$full_name', '$email', '$phone_number', 'not registered')";
-    $result = $conn->query($sql);
-
-    if ($result == 1)
-    {
-        $data["message"] = "data saved successfully";
-        $data["status"] = "Ok";
-    }
-    else
-    {
-        $data["message"] = $_SERVER['PHP_AUTH_PW'];
-        // $data["message"] = "data not saved successfully";
-        $data["status"] = "error";    
-    }
-}
-else
-{
-    $data["message"] = "Format not supported";
-    $data["status"] = "error";    
-}
-    echo json_encode($data);
-
-
-$conn->close();
 ?> 
