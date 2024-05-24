0669b2ca32d8ecab1c1664277be43058d72f96bf
mohamedAbdullaAlNahtta <mohamedAbdullaAlNahtta@mohamed.abdulla.1@gmail.com>
2024-05-24 10:06:46 +0200

    done

--- classes/commissionSystem.class.php
+++ classes/commissionSystem.class.php
@@ -51,8 +51,117 @@ class CommissionSystem{
         
         $result = $commissiondb->query($sql);
         return $result;
-        $userdb->close_db_connection();  
+        $commissiondb->close_db_connection();  
         
+    }
+
+
+    public function get_emp_commission_by_count_value($unit_price, $empArray, $Precentage, $Value){
+
+
+        //employee array should have indexed array for the employee id 
+        $Count_of_employee= count($empArray);
+
+        // if isset the precantage
+        if($Precentage!==NULL){
+            $emp_shares = $unit_price*$Precentage/$Count_of_employee;
+
+            foreach ($empArray as $x) {
+                $emp_share[$x]=$emp_shares;
+              }
+            return $emp_share;
+
+        }else{
+            $emp_shares = $Value/$Count_of_employee;
+
+            foreach ($empArray as $x) {
+                $emp_share[$x]=$emp_shares;
+              }
+
+            return $emp_share;
+        }
+
+    }
+
+    protected function get_emp_manger($emp_id){
+        $commissiondb = new CommissionSystemDB;
+        
+        $sql = "SELECT `manger_id` FROM `employee` WHERE `id`={$emp_id}";
+        
+        $result = $commissiondb->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              $manger_id = $row["manger_id"];
+
+            }
+          } else {
+            $manger_id = NULL;
+          }
+
+        return $manger_id;
+        $commissiondb->close_db_connection();
+
+    }
+
+    public function get_emp_name_by_id($emp_id){
+
+        $commissiondb = new CommissionSystemDB;
+        
+        $sql = "SELECT `name` FROM `employee` WHERE `id`={$emp_id}";
+        
+        $result = $commissiondb->query($sql);
+        while($row = $result->fetch_assoc()) {
+            $emp_name = $row["name"];
+          }
+        return $emp_name;
+        $commissiondb->close_db_connection();
+
+
+
+    }
+
+    public function get_root_emp_manager_array($emp_id){
+
+        $commissiondb = new CommissionSystemDB;
+        
+        $sql = "SELECT `level` FROM `employee` WHERE `id`={$emp_id}";
+        
+        $result = $commissiondb->query($sql);
+
+        while($row = $result->fetch_assoc()) {
+            $emp_level = (int)$row["level"];
+        }
+      
+        $emp_manager_root_array=array($emp_id);
+        $start_emp_id = $emp_id;
+
+        for ($x = $emp_level; $x >= 0; $x--) {
+
+            $emp_manger_id = $this->get_emp_manger($start_emp_id);
+            array_push($emp_manager_root_array, $emp_manger_id);
+            $start_emp_id = $emp_manger_id;
+
+        }
+
+        // remove Null from array 
+        // $del_val = NULL;
+        $count_emp_manager_root_array = count($emp_manager_root_array);
+
+        for ($x = 0; $x <= $count_emp_manager_root_array; $x++) {
+            if(($key=array_search(NULL, $emp_manager_root_array))!==false){
+                unset($emp_manager_root_array[$key]);
+            } 
+        }
+
+        return $emp_manager_root_array;
+        $userdb->close_db_connection();
+    }
+
+    public function remove_duplicated_value($arr){
+        return array_unique($arr));
+
     }
 
 
--- test2.php
+++ test2.php
@@ -6,4 +6,24 @@ include("includes/classautoloader.inc.php");
 
 $newCommissionSystem = new CommissionSystem;
 // $result = $newCommissionSystem->get_sold_units();
+
+
+$cars=array("Volvo","BMW","Toyota","Tyota");
+var_dump($newCommissionSystem->get_emp_commission_by_count_value("1000000", $cars, "0.02", NULL));
+// result should be like this 
+// array(4) { ["Volvo"]=> float(5000) ["BMW"]=> float(5000) ["Toyota"]=> float(5000) ["Tyota"]=> float(5000) } 
+echo "<br>";
+
+// var_dump(get_emp_commission_by_count_value("1000000", $cars, NULL, 2000));
+echo "<br>";
+// result should be like this 
+// array(4) { ["Volvo"]=> int(500) ["BMW"]=> int(500) ["Toyota"]=> int(500) ["Tyota"]=> int(500) } 
+echo "<br>";
+
+var_dump($newCommissionSystem->get_emp_manger(2));
+echo "<br>";
+// var_dump($newCommissionSystem->get_emp_name_by_id(4));
+echo "<br>";
+var_dump($newCommissionSystem->get_root_emp_manager_array(12));
+
 ?>
