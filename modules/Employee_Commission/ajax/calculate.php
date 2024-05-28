979d01182cf9722ed8c65202f2818fefdb164100
Muahmmad Abdullah ElNahtta <38894547+mohamedAbdullaAlNahtta@users.noreply.github.com>
2024-05-28 10:53:21 +0200

    done

--- modules/Employee_Commission/ajax/calculate.php
+++ modules/Employee_Commission/ajax/calculate.php
@@ -1,4 +1,60 @@
 <?php
+/////////////////////////////////////////////////////////////////////////////////
+// Powered by ENG Muhammad Abdullah El Nahtta 
+// This Class auto loader created from scratch by Powered by ENG Muhammad Abdullah El Nahtta
+// Powered by ENG Muhammad Abdullah El Nahtta 
+/////////////////////////////////////////////////////////////////////////////////
+// you can contact me through mobile number 201093001070 or lanline 20 48 2327352
+/////////////////////////////////////////////////////////////////////////////////
+//                             &&&&&&&&&&&&&&&&,@                             
+//                           &&&&&&&&&&&&&&&&&@&&&%&%                          
+//                         *%&%&&%%%%#%%%%%%&%&&&&&&&                          
+//                         &&&/,,,,,,...*,*,,***//(&&&%                        
+//                       %&%&,,.....,,,,,,,,,,****//%&&                        
+//                       %%#*,..... .........,,,,***/#&&                       
+//                       &#*...........,...,,,,,,,****%&                       
+//                       %(,....,...,......,,,###((***/&&*                     
+//                     ..,#*,&#/,****@&*,,%/((#((/((*@#*,*                     
+//                     .,..,@,,*(.&@(/,/.,@/((((*//*,*%**/                     
+//                       ,,(,/..,,**,./..,**@,**,%&***//*                      
+//                        .(,....,,,.....,******,,,,*/#,.                      
+//                          (....,(...*..,*//***/#*//%                         
+//                          %#,,//#(///((((#*//%%&/(%&                         
+//                           #/#/*(..,,,,*,*//**#/%&&.                         
+//                            /%#*/,...,,///****#&&&                           
+//                             ,*%%#/(*//#(###%&&#/*,                          
+//                            ,&..*/&&%&&&&&&&&#***,.&&                        
+//                          @%   (....,,*///*****,  ..&&&&&&&&&&@              
+//                     &&&&&%%%     *...,,,*,,,    ...&&&&&&&&&&&&&&&&&&&&&    
+//             &%%%%%%%&&&%%%%%         ,,,,        .,&&&&&&&&&&&&&&&&&&&&&&&&&
+//       %%%&&&%%%%%%%%%%&%%%%%%      @&#%%&&&&%  ...,&&&&&&&%&&&&%%%&&%&&&&&&&
+//    %&&&%%%%%%%###%%%#%%%%%%%.     ,(/%#&&&&,.,# ...(&&&&&&&%&&&%%%%%%%%%%%&&
+//    %%%%%%%#######%####%%%%%%%   ... *%#&&&,....   .*&&&&%&%%&&&&&%%%%%%%%%&&
+//    %%################%%%%%%%&.        %%%&,....    /&&&%&%%%%&&&&&%%%%&&%&&%
+//    %%###(#(((#(#(####%%%%%%%&,       %%%&&&...     .&&&&%%%%%&&&&&&&&&&&&&&&
+/////////////////////////////////////////////////////////////////////////////////
+// you can contact me through mobile number 201093001070 or lanline 20 48 2327352
+/////////////////////////////////////////////////////////////////////////////////
+// Powered by ENG Muhammad Abdullah El Nahtta 
+// This Class auto loader created from scratch by Powered by ENG Muhammad Abdullah El Nahtta
+// Powered by ENG Muhammad Abdullah El Nahtta 
+/////////////////////////////////////////////////////////////////////////////////
+
+spl_autoload_register("myAutoLoader");
+function myAutoLoader($className){
+    $server = $_SERVER['DOCUMENT_ROOT'];
+    $path = "$server/El-Masria-Group/classes/";
+    $extensiontype = ".class.php";
+    $fullpath = $path . $className . $extensiontype;
+
+	include $fullpath;
+
+}
+
+session_start();
+error_reporting(0);  
+
+$newCommissionSystem = new CommissionSystem;
 
 $salesText = $_GET['salesText'];
 $contractText = $_GET['contractText'];
@@ -16,5 +72,10 @@ echo"<br>";
 var_dump($operationArr);
 echo"<br>";
 
-
+var_dump($newCommissionSystem->calculate_Sales_commission($salesArr, "1000000", "West", "no", "yes"));
+echo"<br>";
+var_dump($newCommissionSystem->calculate_Contract_commission($contractArr, "1000000", "West", "yes", "yes"));
+echo"<br>";
+var_dump($newCommissionSystem->calculate_operation_commission($operationArr, "1000000", "East", "no", "yes"));
+echo"<br>";
 ?>
